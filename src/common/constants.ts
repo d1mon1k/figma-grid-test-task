7f58c8f7727cc75a8eb99af99dd5c28815289c06
Artsiom Ostrovskiy <ostrovskiy.a.97@gmail.com>
2023-08-14 15:03:57 +0200

    chore: add constants

---
+++ src/common/constants.ts
@@ -0,0 +1,8 @@
+export const initialCellSize = 6;
+export const initialRulerStep = 50;
+
+export const figmaGridWidth = 264 * 1000 + 142;
+export const figmaGridHeight = 264 * 1000 + 144;
+
+export const rowCount = figmaGridHeight / initialCellSize;
+export const columnCount = figmaGridWidth / initialCellSize;
