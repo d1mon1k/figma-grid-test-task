0e9914027ceefc904291df06873c4313d3e882fe
Artsiom Ostrovskiy <ostrovskiy.a.97@gmail.com>
2023-08-14 17:31:21 +0200

    feat: add getColumnOffset & getRowOffset grid helpers

--- src/helpers/grid.ts
+++ src/helpers/grid.ts
@@ -3,3 +3,10 @@ export const getEstimatedTotalHeight = (rowCount: number, cellSideSize: number) 
 
 export const getEstimatedTotalWidth = (columnCount: number, cellSideSize: number) =>
   cellSideSize * columnCount;
+
+export const getColumnOffset = (cellSideSize: number, index: number): number =>
+  cellSideSize * index;
+
+// prettier-ignore
+export const getRowOffset = (cellSideSize: number, index: number): number =>
+  cellSideSize * index;
